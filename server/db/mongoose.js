f477990ea8a1317d03a00eb78ac7cb7d6404d0e1
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-08 18:56:23 +0530

    mlab test db added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{useMongoClient: true});
+mongoose.connect('mongodb://ithebk:brtmlb16&#@ds251845.mlab.com:51845/todo-app',{useMongoClient: true});
 
 module.exports = {mongoose};
