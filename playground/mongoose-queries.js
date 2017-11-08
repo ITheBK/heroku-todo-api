862f684165bbc56cfd3178e7cb8ed08319f05a6e
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-08 17:31:54 +0530

    Add queries for playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,35 @@
+  const{ObjectID} = require('mongodb');
+  const {mongoose} = require('./../server/db/mongoose');
+  const {Todo} = require('./../server/models/todos');
+  const {User} = require('./../server/models/users');
+
+  var id = '5a02d76007a15d2d040f6d9a';
+  if(!ObjectID.isValid(id)){
+    console.log("Invalid Id");
+  }
+  // Todo.find({
+  //   _id:id
+  // }).then((todos) => {
+  //   console.log('Todos',todos);
+  // });
+  //
+  // Todo.findOne({
+  //     _id:id
+  // }).then((todo) => {
+  //   console.log('Todo',todo);
+  // });
+
+  // Todo.findById(id).then((todo) => {
+  //   if(!todo) {
+  //     return console.log('Id not found');
+  //   }
+  //   console.log('Todo by Id',todo);
+  // }).catch((e) => console.log(e));
+
+  var userId = '5a02a7a27b70ab088a37f1e8';
+  User.findById(userId).then((user) => {
+    if(!user){
+      return console.log('User not found');
+    }
+    console.log(user);
+  }).catch((e) => console.log(e));
