c7da6a2ada0982b3d915ee7f5b980aeb3b63f6dc
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-16 11:03:31 +0530

    Last commit

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@
 
   //Mongo from mlab :
   /*
-  >heroku config:set MONGODB_URI="mongodb://ithebk:brtmlb16&#@ds251845.mlab.com:51845/todo-app"
+  >heroku config:set MONGODB_URI="mongodb://ithebk:password@ds251845.mlab.com:51845/todo-app"
   **/
   if(env === 'development' || env === 'test') {
     var config = require('./config.json');
